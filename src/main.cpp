1e123609c580eb108983f96ee1180a6e1e446d29
Aphrim <gregperrett5@gmail.com>
2022-01-28 22:06:46 -0500

    Initial commit.

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include <glad/glad.h> 
+#include <GLFW/glfw3.h>
+
+
+int main() {
+    //Window creation
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Window", NULL, NULL);
+    if (window == nullptr)
+    {
+        std::cout << "Window failed to create" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "GL failed to load" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    return 0;
+}
